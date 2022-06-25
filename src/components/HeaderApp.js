4633974f37b8ad6fedcbe148efba76be4336f947
danielorozco14 <00200617@uca.edu.sv>
2022-06-25 15:43:02 -0600

    Cambio en el header

--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -4,14 +4,20 @@ export const HeaderApp = () => {
     return (
         <>
             <header>
-                <h1>Proyecto Administracion de riesgos informaticos</h1>
-                <h2>Integrantes: </h2>
-                <ul className='list-group'>
-                    <li className='list-group-item'>Alexis Mancia #00305018</li>
-                    <li className='list-group-item'>Daniel Orozco #00200617</li>
-                    <li className='list-group-item'>Moises Rosales #00218916</li>
-                    <li className='list-group-item'>Lourdes Moran #00113417</li>
-                </ul>
+                <h1>Proyecto Administracion de Riesgos Informaticos</h1>
+                <div className="card">
+                    <div className="card-header">
+                        <h2>Integrantes: </h2>
+                    </div>
+                    <div className='card-body'>
+                        <ul className='list-group'>
+                            <li className='list-group-item'>Alexis Mancia #00305018</li>
+                            <li className='list-group-item'>Daniel Orozco #00200617</li>
+                            <li className='list-group-item'>Moises Rosales #00218916</li>
+                            <li className='list-group-item'>Lourdes Moran #00113417</li>
+                        </ul>
+                    </div>
+                </div>                
             </header>
         </>
     )
