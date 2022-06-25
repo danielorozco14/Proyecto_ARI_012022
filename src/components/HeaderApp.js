9bb850ee357c6de4e9b024b8e4b17b9097f68bbb
danielorozco14 <00200617@uca.edu.sv>
2022-06-25 15:26:14 -0600

    Numeros de carnet agregados

--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -9,8 +9,8 @@ export const HeaderApp = () => {
                 <ul className='list-group'>
                     <li className='list-group-item'>Alexis Mancia #00305018</li>
                     <li className='list-group-item'>Daniel Orozco #00200617</li>
-                    <li className='list-group-item'>Moises Rosales #</li>
-                    <li className='list-group-item'>Lourdes Moran #</li>
+                    <li className='list-group-item'>Moises Rosales #00218916</li>
+                    <li className='list-group-item'>Lourdes Moran #00113417</li>
                 </ul>
             </header>
         </>
